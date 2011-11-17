06a5272afb3788827d69121f40cd2572a786d266
bastinat0r <bastinat0r@bastinat0r.de>
2011-11-17 14:48:14 +0100

    Message.java

---
+++ Message/src/Message.java
@@ -0,0 +1,46 @@
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author sebastian
+ */
+public class Message implements Serializable {
+
+	Date timeStamp;
+	String text;
+	String username;
+	
+	public Message(Message m)
+	{
+		timeStamp = m.getTimeStamp();
+		text = m.getText();
+		username = m.getUsername();
+	}
+	
+	public Message(String username, String text)
+	{
+		timeStamp = new Date();
+		this.text = text;
+		this.username = username;
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String toString()
+	{
+		return timeStamp.toString() + " " + username + " : " + text;	
+	}
+	
+}
