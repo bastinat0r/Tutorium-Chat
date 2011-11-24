eb095179f97982af5486fa7caa2f483f01dc1b81
Rosario Raulin <post@rosario-raulin.de>
2011-11-24 21:34:07 +0100

    do some neat error handling

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -1,10 +1,10 @@
 import java.net.Socket;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
-import java.io.IOException;
 
-// This is just a dummy yet. You can't compile it because some code is still
-// missing. Look at the TODOs to see which parts these are.
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.StreamCorruptedException;
 
 public class ClientHandler extends Thread {
     private Socket socket;
@@ -13,6 +13,8 @@ public class ClientHandler extends Thread {
     private boolean loggedIn = false;
     private SharedSecrets db;
 
+    private boolean run = true; // used to indicate that thread the should stop
+
     public ClientHandler(Socket socket, SharedSecrets db) {
 	this.socket = socket;
 	this.db = db;
@@ -20,10 +22,28 @@ public class ClientHandler extends Thread {
 	    out = new ObjectOutputStream(socket.getOutputStream());
 	    in = new ObjectInputStream(socket.getInputStream());
 	}
-	catch(IOException e) {
-	    System.err.println("error in ClientHandler(): " + e);
+	catch(StreamCorruptedException e) {
+	    run = false;
+	    System.err.println("error in ClientHandler(): user has no valid client (" + socket.getInetAddress() + ")");
+	    
+	    try {
+		socket.getOutputStream().write("error: You are not using a valid client.\r\n".getBytes());
+		socket.getOutputStream().flush();
+	    }
+	    catch(Exception writeError) {
+		System.err.println("error in ClientHandler(): Could not even send error message to " + socket.getInetAddress());
+	    }
+	}
+	catch(EOFException e) {
+	    run = false;
+	    System.err.println("error in ClientHandler(): Client closed the connection (" + socket.getInetAddress() + ")");
+	}
+	catch(Exception e) {
+	    run = false;
+	    System.err.println("unknown error in ClientHandler(): " + e);
 	}
     }
+
 
     public void process(AuthMessage m) throws Exception {
 	AuthMessage random = new AuthMessage(m.getUsername(), 1024);
@@ -48,23 +68,16 @@ public class ClientHandler extends Thread {
     
     public void run() {
 	try {
-	    while(true) {
-		// Does polymorphism work here? Does the object know what it
-		// *really* is?
+	    while(run) {
 		Message inp = (Message)in.readObject();
 		if(inp instanceof AuthMessage) {
 		    process((AuthMessage)inp);
 		}
 	    }
+	    socket.close();
 	}
 	catch(Exception e) { // TODO: more precise error handling
 	    System.err.println("error in run(): " + e);
-	    try {
-		socket.close();
-	    }
-	    catch(Exception error) {
-		System.err.println("Could not close!");
-	    }
 	}
     }
 }
