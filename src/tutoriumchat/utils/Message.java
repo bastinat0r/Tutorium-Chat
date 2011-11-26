1e54c2d4c790155d3af99a04ee90fc23ac4c59a5
Dmitry Russ <dr@travelping.com>
2011-11-26 22:59:20 +0100

    Autoformat some code in Messages classes.

--- src/tutoriumchat/utils/Message.java
+++ src/tutoriumchat/utils/Message.java
@@ -4,44 +4,45 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
- *
+ * 
  * @author sebastian
  */
 public abstract class Message implements Serializable {
 
-	Date timeStamp;
-	String text;
-	String username;
-	
-	public Message(Message m)
-	{
-		timeStamp = m.getTimeStamp();
-		text = m.getText();
-		username = m.getUsername();
-	}
-	
-	public Message(String username, String text)
-	{
-		timeStamp = new Date();
-		this.text = text;
-		this.username = username;
-	}
-	
-	public String getText() {
-		return text;
-	}
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    Date timeStamp;
+    String text;
+    String username;
 
-	public Date getTimeStamp() {
-		return timeStamp;
-	}
+    public Message(Message m) {
+        timeStamp = m.getTimeStamp();
+        text = m.getText();
+        username = m.getUsername();
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public Message(String username, String text) {
+        timeStamp = new Date();
+        this.text = text;
+        this.username = username;
+    }
 
-	public String toString()
-	{
-		return timeStamp.toString() + " " + username + " : " + text;	
-	}
-	
+    public String getText() {
+        return text;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String toString() {
+        return timeStamp.toString() + " " + username + " : " + text;
+    }
+
 }
