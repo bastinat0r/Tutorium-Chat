78e7a93da9d9eedf0b90b7e20066bf389952a55b
Dmitry Russ <dr@travelping.com>
2011-11-26 10:38:29 +0100

    Add configurable port for start application.

--- src/tutoriumchat/server/ServerStart.java
+++ src/tutoriumchat/server/ServerStart.java
@@ -1,10 +1,12 @@
 package tutoriumchat.server;
 
 public class ServerStart {
-	public class ChatServerTest {
-	    public static void main(String[] args) {
-		Server s = new Server(8080);
-		s.listen();
-	    }
+	public static void main(String[] args) {
+	    
+		if(args.length > 0){
+			int port = Integer.parseInt( args[0] );
+			new Server(port);
+			} else new Server(8080);
+		}
 	}
 }
