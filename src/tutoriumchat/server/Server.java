cfa25ff908053b772ee1617074671a26b4c00b32
Dmitry Russ <dr@travelping.com>
2011-11-26 15:12:50 +0100

    Add HashTable to save ObjectOutputStreams.

--- src/tutoriumchat/server/Server.java
+++ src/tutoriumchat/server/Server.java
@@ -1,19 +1,27 @@
 package tutoriumchat.server;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
 
-public class Server {
+import tutoriumchat.utils.*;
 
+public class Server{
+	
 	private ServerSocket socket;
+    private Hashtable connectionmap;
 	private SharedSecrets db;
 
-	public ChatServer(int port) {
+	public Server(int port) {
 		try {
 			socket = new ServerSocket(port);
 		} catch (IOException e) {
 			System.err.println("error in ChatServer(): " + e);
 		}
+		connectionmap = new Hashtable<Socket, ObjectOutputStream>();
+		listen();
 	}
 
 	public void listen() {
@@ -21,10 +29,15 @@ public class Server {
 			try {
 				// TODO: We should keep track of clients so we can send
 				// messages to them
-				new ClientHandler(socket.accept(), db).start();
+				Socket newconnection = socket.accept();
+				new ClientHandler(newconnection, this, db);
+				ObjectOutputStream newstream = new ObjectOutputStream(socket.getOutputStream());
+				connectionmap.put(newconnection, newstream);
 			} catch (IOException e) {
 				System.err.println("error in void listen(): " + e);
 			}
 		}
+	
+	public void message()
 	}
 }
