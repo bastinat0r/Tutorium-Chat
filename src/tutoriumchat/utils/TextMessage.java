f7e1dc5f62991c7946337678a8a80cc8dd76cbb9
Dmitry Russ <dr@travelping.com>
2011-11-27 09:03:17 +0100

    Implements TextMessage(ha ha)

---
+++ src/tutoriumchat/utils/TextMessage.java
@@ -0,0 +1,14 @@
+package tutoriumchat.utils;
+
+public class TextMessage extends Message {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public TextMessage(Message m) {
+        super(m);
+    }
+
+}
