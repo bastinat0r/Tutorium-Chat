b741bcc24ad79995c244a4b11de3880b19e7ad10
Dmitry Russ <dr@travelping.com>
2011-11-27 09:02:50 +0100

    Add AuthorisationException.

---
+++ src/tutoriumchat/server/utils/AuthorisationException.java
@@ -0,0 +1,13 @@
+package tutoriumchat.server.utils;
+
+public class AuthorisationException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3699473336533099185L;
+
+    public AuthorisationException(String s) {
+        super(s);
+    }
+}
